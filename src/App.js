d405d5a4853b4b4663242968965764f8037b8301
olms <olms2319@outlook.com>
2022-10-12 12:47:53 +0800

    解包前

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Outlet} from 'react-router-dom'
+import './assets/base.css'
+import { Button } from 'antd'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button type="primary" block>
+      Primary
+    </Button>
+    <Outlet/>
     </div>
-  );
+  )
 }
-
-export default App;
